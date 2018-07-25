1bab7ff45150833579c583ea431c6b860bed332d
shingotomoto <t0m0t05h1n90@gmail.com>
2018-07-25 16:15:13 +0900

    test

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :iOS, '9.0'
+
+target 'osagari' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+   pod 'Firebase'
+   pod 'Firebase/Database'
+   pod 'Firebase/Auth'
+
+  # Pods for osagari
+
+end
---
+++ osagari/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  osagari
+//
+//  Created by Shingo Tomoto on 2018/07/24.
+//  Copyright © 2018年 Shingo Tomoto. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailtextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+ 
+    }
+    
+    @IBAction func createNewUser(_ sender: Any) {
+        //    新規ユーザ登録
+        
+        
+    }
+
+    
+    @IBAction func userLogin(_ sender: Any) {
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+ 
+    }
+    
+
+   
+
+}
